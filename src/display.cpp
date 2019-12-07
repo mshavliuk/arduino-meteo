a28b1e306e176edaf3f171628359a680b4afa865
mshavliuk <m.shavliuk@beauit.com>
2019-12-07 13:07:36 +0200

    advanced display functions

--- src/display.cpp
+++ src/display.cpp
@@ -1,24 +1,41 @@
 #pragma once
 #include <LiquidCrystal_I2C.h>
 #include <vector>
+#include <array>
+#include <set>
+
+#include "logger.cpp"
 
 #define DISPLAY_ADDR 0x27
+#define SPACE 32
+#define FILL 255
 
-uint8_t LT[8] = {0b00000,  0b00000,  0b00000,  0b00000,  0b00000,  0b00000,  0b11111,  0b11111};
-uint8_t UB[8] = {0b11111, 0b11111, 0b11111, 0b00000, 0b00000, 0b00000, 0b00000,
-                 0b00000};   // для двустрочных 7, 0   // для четырехстрочных 2, 3, 4, 5, 6, 8, 9
-uint8_t UMB[8] = {0b11111, 0b11111, 0b11111, 0b00000, 0b00000, 0b00000, 0b11111,
-                  0b11111};  // для двустрочных 2, 3, 5, 6, 8, 9
-uint8_t LMB[8] = {0b11111, 0b00000, 0b00000, 0b00000, 0b00000, 0b11111, 0b11111,
-                  0b11111};  // для двустрочных 2, 3, 5, 6, 8, 9
-uint8_t LM2[8] = {0b11111, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000};  // для двустрочной 4
-uint8_t LB[8] = {0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b11111, 0b11111, 0b11111};   // для 0
+typedef std::vector<std::vector<uint8_t>> byte_matrix;
+typedef std::array<uint8_t, 8> char_tile;
+
+uint8_t LT[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1F, 0x1F};
+uint8_t UB[8] = {0x1F, 0x1F, 0x1F, 0x00, 0x00, 0x00, 0x00, 0x00};
+uint8_t UMB[8] = {0x1F, 0x1F, 0x1F, 0x00, 0x00, 0x00, 0x1F, 0x1F};
+uint8_t LMB[8] = {0x1F, 0x00, 0x00, 0x00, 0x00, 0x1F, 0x1F, 0x1F};
+uint8_t LM2[8] = {0x1F, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+uint8_t LB[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x1F, 0x1F, 0x1F};
+
+
+const char_tile basicChars[] = {
+        {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1F, 0x1F},
+        {0x1F, 0x1F, 0x1F, 0x00, 0x00, 0x00, 0x00, 0x00},
+        {0x1F, 0x1F, 0x1F, 0x00, 0x00, 0x00, 0x1F, 0x1F},
+        {0x1F, 0x00, 0x00, 0x00, 0x00, 0x1F, 0x1F, 0x1F},
+        {0x1F, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+        {0x00, 0x00, 0x00, 0x00, 0x00, 0x1F, 0x1F, 0x1F}
+};
+const char_tile fillTile = {0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F};
 
 
 class Display {
 public:
-    Display() : lcd(DISPLAY_ADDR, 20, 4) {
-        Serial.println("init display");
+    Display() : lcd(DISPLAY_ADDR, 20, 4), logger("Display") {
+        this->logger.info("init display");
         this->lcd.init();
         this->lcd.backlight();
     }
@@ -35,16 +52,33 @@ public:
         } else if (mode == BIG) {
             this->setBigDigits();
 
-            Serial.println(string);
+            this->logger.info("Writing big chars:", string);
+            uint8_t shift = 0;
             for (uint8_t cur = 0; string[cur] != '\0'; cur++) {
-                this->writeBigDigit(string[cur], x + cur * 4, y);
+                shift += this->writeBigDigit(string[cur], x + shift, y);
+//                delay(1000);
             }
         }
     }
 
 private:
     LiquidCrystal_I2C lcd;
+    Logger logger;
 
+    struct TileComparator {
+        bool operator() (const char_tile& a, const char_tile& b) const {
+            for(auto i = 0; i < (uint8_t) sizeof(char_tile); i++) {
+                if(a.at(i) < b.at(i)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    };
+
+
+    void getExtraCharSet() {}
+    
     void setBigDigits() {
         this->lcd.createChar(0, LT);
         this->lcd.createChar(1, UB);
@@ -52,68 +86,155 @@ private:
         this->lcd.createChar(3, UMB);
         this->lcd.createChar(4, LMB);
         this->lcd.createChar(5, LM2);
+        this->lcd.createChar(6, LM2);
     }
 
-    void digSeg(byte x, byte y, std::vector<std::vector<byte>>& charBytes) {
+    uint8_t drawByBytesMatrix(uint8_t x, uint8_t y, byte_matrix& charBytes) {
+        uint8_t width = 0;
         for(auto& row : charBytes) {
             lcd.setCursor(x, y);
             for(auto& charByte : row) {
                 lcd.write(charByte);
             }
-            lcd.write(32); // only for x > 11
+            width = width > row.size() ? width : row.size();
             y++;
         }
-//        if (x <= 11) {
-//            lcd.print(" ");
-//        }
+        return width;
     }
 
-    void writeBigDigit(char dig, byte x, byte y) {
-        std::vector<std::vector<byte>> charBytes;
-        switch (dig) {
-            case '0':
-                charBytes = {{255, 1, 255}, {255, 2, 255}};
-                this->digSeg(x, y, charBytes);
-                break;
-            case '1':
-                charBytes = {{32, 255, 32}, {32, 255, 32}};
-                this->digSeg(x, y, charBytes);
-                break;
-            case '2':
-                charBytes = {{3, 3, 255}, {255, 4, 4}};
-                this->digSeg(x, y, charBytes);
-                break;
-            case '3':
-                charBytes = {{3, 3, 255}, {4, 4, 255}};
-                this->digSeg(x, y, charBytes);
-                break;
-            case '4':
-                charBytes = {{255, 0, 255}, {5, 5, 255}};
-                this->digSeg(x, y, charBytes);
-                break;
-            case '5':
-                charBytes = {{255, 3, 3}, {4, 4, 255}};
-                this->digSeg(x, y, charBytes);
-                break;
-            case '6':
-                charBytes = {{255, 3, 3}, {255, 4, 255}};
-                this->digSeg(x, y, charBytes);
-                break;
-            case '7':
-                charBytes = {{1, 1, 255}, {32, 255, 32}};
-                this->digSeg(x, y, charBytes);
-                break;
-            case '8':
-                charBytes = {{255, 3, 255}, {255, 4, 255}};
-                this->digSeg(x, y, charBytes);
-                break;
-            case '9':
-                charBytes = {{255, 3, 255}, {4, 4, 255}};
-                this->digSeg(x, y, charBytes);
-                break;
-            default:
-                Serial.println("Big char is not supported");
-                Serial.println(dig);
+    void clearSectors(uint8_t x, uint8_t y, uint8_t height, uint8_t width) {
+        for (auto j = y; j < y + height; j++) {
+            lcd.setCursor(x, j);
+            for (auto i = 0; i < width; i++) {
+                lcd.write(' ');
+            }
         }
+    }
+
+    std::set<char_tile, TileComparator> makeBigCharThinner(byte_matrix& matrix) {
+        std::set<char_tile, TileComparator> extraChars;
+
+        auto applyBitMask = [](const char_tile charTile, uint8_t mask){
+            char_tile result;
+            for(uint8_t i = 0; i < (uint8_t) sizeof(char_tile); i++) {
+//                result[i] = charTile.at(i) & mask;
+                result[i] = charTile[i];
+            }
+            return result;
+        };
+
+        for(auto& row : matrix) {
+            for(size_t i = 0; i < row.size(); i++) {
+                char_tile tile;
+                if (row[i] == FILL) {
+                    tile = fillTile;
+                } else {
+                    tile = basicChars[row[i]];
+                }
+
+                if (row[i] > sizeof(basicChars) && row[i] != FILL) {
+                    continue;
+                } else if (i == row.size() - 1 || row[i + 1] == SPACE) {
+                    extraChars.insert(applyBitMask(tile, 0x1Eu));
+                } else if (i == 0 || row[i - 1] == SPACE) {
+                    extraChars.insert(applyBitMask(tile, 0x0Fu));
+                } else {
+                    extraChars.insert(tile);
+                }
+                row[i] = extraChars.size() - 1;
+            }
+        }
+
+        return extraChars;
+    }
+
+    byte_matrix getBigCharByteMatrix(char character) {
+        switch (character) {
+            case '0':
+                return {{FILL, 1, FILL},
+                        {FILL, 2, FILL}};
+            case '1':
+                return {{SPACE, FILL, SPACE},
+                        {SPACE, FILL, SPACE}};
+            case '2':
+                return {{3,   3, FILL},
+                        {FILL, 4, 4}};
+            case '3':
+                return {{3, 3, FILL},
+                        {4, 4, FILL}};
+            case '4':
+                return {{FILL, 0, FILL},
+                        {5,   5, FILL}};
+            case '5':
+                return {{FILL, 3, 3},
+                        {4,   4, FILL}};
+            case '6':
+                return {{FILL, 3, 3},
+                        {FILL, 4, FILL}};
+            case '7':
+                return {{1,  1,   FILL},
+                        {SPACE, FILL, SPACE}};
+            case '8':
+                return {{FILL, 3, FILL},
+                        {FILL, 4, FILL}};
+            case '9':
+                return {{FILL, 3, FILL},
+                        {4,   4, FILL}};
+            case ':':
+                return {{165},
+                        {165}};
+            case ' ':
+                return {{SPACE},
+                        {SPACE}};
+            default:
+                this->logger.error("Big char is not supported:", character);
+                return {{63},
+                        {63}};
+        }
+    }
+
+    void setExtraTiles(const std::set<char_tile, TileComparator>& tiles) {
+//        for(auto tile : tiles) {
+//            for(auto row : tile) {
+//              Serial.print(row, HEX);
+//                Serial.print(' ');
+//            }
+//            Serial.println(' ');
+//        }
+        uint8_t n = 0;
+        for (auto tile : tiles) {
+            Serial.print("new char #");
+            Serial.print(n, DEC);
+            Serial.print(": ");
+            char tileData[8];
+            for(uint8_t i = 0; i < (uint8_t) tile.size(); i++) {
+                Serial.print(tile[i], HEX);
+                Serial.print(" ");
+                tileData[i] = (char) tile[i];
+            }
+
+
+            this->lcd.createChar(n++, tile.data());
+            Serial.println("");
+        }
+    }
+
+    uint8_t writeBigDigit(char character, uint8_t x, uint8_t y) {
+        auto charBytes = this->getBigCharByteMatrix(character);
+
+//        auto tiles = this->makeBigCharThinner(charBytes);
+//        this->makeBigCharThinner(charBytes);
+
+//        Serial.println('Char thinner tile: ');
+//        for(auto tile : tiles) {
+//            for(auto row : tile) {
+//              Serial.print(row, HEX);
+//                Serial.print(' ');
+//            }
+//            Serial.println(' ');
+//        }
+//        this->setExtraTiles(tiles);
+        uint8_t width = this->drawByBytesMatrix(x, y, charBytes);
+        return width;
     }
 };
